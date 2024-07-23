c508ba7459252635636145a0233bc65df027a4dd
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-23 16:12:18 +0200

    Remove and delegate paper updation and deletion to admin

--- backend/src/api/routes/v1/paper.routes.ts
+++ backend/src/api/routes/v1/paper.routes.ts
@@ -3,25 +3,29 @@ import { isAuthenticated } from '../../middleware/isAuthenticated';
 import { isAuthorized } from '../../middleware/isAuthorized';
 import {
   addPapper,
-  deletePaper,
   getBookmarksByUser,
   getPaperById,
   getPapers,
   searchPapers,
-  updatePaper,
 } from '../../controllers/paper.controler';
 import { toogleBookmark } from '@/api/models/paper.model';
+import { userRole } from '@/api/middleware/userRole';
 
 const router = Router();
 
-router.post('/add', isAuthenticated, isAuthorized, addPapper);
+router.post(
+  '/add',
+  isAuthenticated,
+  isAuthorized,
+  userRole('admin'),
+  addPapper
+);
 router.get('/papers', getPapers);
 router.get('/paper:id', getPaperById);
 router.post('/search', searchPapers);
 router.post('/bookmark', isAuthenticated, isAuthorized, toogleBookmark);
 router.get('/bookmarks', isAuthenticated, isAuthorized, getBookmarksByUser);
-router.put('/update:id', isAuthenticated, isAuthorized, updatePaper);
-router.delete('/delete:id', isAuthenticated, isAuthorized, deletePaper);
+
 
 const paperRoutes = router;
 
--- backend/src/api/routes/v1/user.routes.ts
+++ backend/src/api/routes/v1/user.routes.ts
@@ -3,21 +3,15 @@ import { isAuthenticated } from '../../middleware/isAuthenticated';
 import { isAuthorized } from '../../middleware/isAuthorized';
 import {
   addUser,
-  deleteUser,
   getUserById,
-  getUsers,
   loginUser,
-  updateUser,
 } from '../../controllers/user.controller';
 
 const router = Router();
 
 router.post('/create', addUser);
 router.post('/login', loginUser);
-router.get('/users', getUsers);
 router.get('/user:id', isAuthenticated, isAuthorized, getUserById);
-router.put('/update:id', isAuthenticated, isAuthorized, updateUser);
-router.delete('/delete:id', isAuthenticated, isAuthorized, deleteUser);
 
 const userRoutes = router;
 
