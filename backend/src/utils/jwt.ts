888abbbb901a148b5841df5b6a66001121394352
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-23 14:27:34 +0200

    feat: Implement jwt token generation and verification for user

---
+++ backend/src/utils/jwt.ts
@@ -0,0 +1,40 @@
+import { env } from '@config/env';
+import * as jwt from 'jsonwebtoken';
+import { User, ResponseData } from '../types';
+import { getUserById } from '@/api/models/user.model';
+
+export async function generateJwtToken(user: Partial<User>): Promise<string> {
+  try {
+    const payload = {
+      sub: user.id,
+      iat: Math.floor(Date.now() / 1000),
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: '48h',
+    });
+
+    return token;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function verifyJwtToken(
+  token: string
+): Promise<ResponseData<User>> {
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET) as { sub: string };
+    const userId = decoded.sub;
+
+    const user = await getUserById(userId);
+
+    if (!user) {
+      return { errorMessage: 'User does not exist', success: false };
+    }
+
+    return { value: user, success: true };
+  } catch (error) {
+    return { errorMessage: 'Unable to verify token', success: false };
+  }
+}
