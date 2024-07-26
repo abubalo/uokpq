2753449288d6187e782d2430c6b360174178df87
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-26 18:06:01 +0200

    Add back previously remove route to avoid routes

--- backend/src/api/routes/v1/paper.routes.ts
+++ backend/src/api/routes/v1/paper.routes.ts
@@ -1,31 +1,62 @@
 import { Router } from 'express';
-import { isAuthenticated } from '../../middleware/isAuthenticated';
-import { isAuthorized } from '../../middleware/isAuthorized';
+import { isAuthenticated } from '@/api/middleware/isAuthenticated';
+import { isAuthorized } from '@/api/middleware/isAuthorized';
 import {
   addPapper,
+  archivePaper,
+  deletePaper,
   getBookmarksByUser,
   getPaperById,
   getPapers,
   searchPapers,
-} from '../../controllers/paper.controler';
+  updatePaper,
+} from '@/api/controllers/paper.controler';
 import { toogleBookmark } from '@/api/models/paper.model';
 import { userRole } from '@/api/middleware/userRole';
+import { deleteUser } from '@/api/controllers/user.controller';
 
 const router = Router();
 
+router.get('/', getPapers);
+router.get('/paper:id', getPaperById);
 router.post(
-  '/add',
+  '/add-paper',
   isAuthenticated,
   isAuthorized,
   userRole('admin'),
   addPapper
 );
-router.get('/papers', getPapers);
-router.get('/paper:id', getPaperById);
+router.put(
+  '/update-paper:id',
+  isAuthenticated,
+  isAuthorized,
+  userRole('admin'),
+  updatePaper
+);
+router.put(
+  '/archive-paper:id',
+  isAuthenticated,
+  isAuthorized,
+  userRole('admin'),
+  archivePaper
+);
+router.delete(
+  '/delete-paper:id',
+  isAuthenticated,
+  isAuthorized,
+  userRole('admin'),
+  deletePaper
+);
+router.delete(
+  '/delete-user:id',
+  isAuthenticated,
+  isAuthorized,
+  userRole('admin'),
+  deleteUser
+);
 router.post('/search', searchPapers);
 router.post('/bookmark', isAuthenticated, isAuthorized, toogleBookmark);
 router.get('/bookmarks', isAuthenticated, isAuthorized, getBookmarksByUser);
-
 
 const paperRoutes = router;
 
