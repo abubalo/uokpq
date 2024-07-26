e5362da65dc08177dfaf15bcdb1de8334071df52
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-26 17:38:22 +0200

    Add admin controller

---
+++ backend/src/api/controllers/admin.controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+
+export async function getDashbaord(req: Request, res: Response): Promise<void> {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
