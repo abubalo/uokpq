d4a4fe88582a6d2206120b1d76b5af686ab04f24
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-26 18:10:29 +0200

    refactor: Admin routes

--- backend/src/api/routes/v1/admin.routes.ts
+++ backend/src/api/routes/v1/admin.routes.ts
@@ -1,7 +1,3 @@
-import { deleteUser, getUsers } from '@/api/controllers/user.controller';
-import { archivePaper, deletePaper, updatePaper } from '../../controllers/paper.controler';
-import { isAuthenticated } from '@/api/middleware/isAuthenticated';
-import { isAuthorized } from '@/api/middleware/isAuthorized';
 import { userRole } from '@/api/middleware/userRole';
 import { Router } from 'express';
 
@@ -9,41 +5,6 @@ const router = Router();
 
 router.get('/dashboard', userRole('admin'));
 router.post('/create-admin', userRole('super-admin'));
-router.get(
-  '/get-users:id',
-  isAuthenticated,
-  isAuthorized,
-  userRole('admin'),
-  getUsers
-);
-router.put(
-  '/update-paper:id',
-  isAuthenticated,
-  isAuthorized,
-  userRole('admin'),
-  updatePaper
-);
-router.put(
-  '/archive-paper:id',
-  isAuthenticated,
-  isAuthorized,
-  userRole('admin'),
-  archivePaper
-);
-router.delete(
-  '/delete-paper:id',
-  isAuthenticated,
-  isAuthorized,
-  userRole('admin'),
-  deletePaper
-);
-router.delete(
-  '/delete-user:id',
-  isAuthenticated,
-  isAuthorized,
-  userRole('admin'),
-  deleteUser
-);
 
 const adminRoutes = router;
 
