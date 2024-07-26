d3a4572b326354ebeaf2b86ae540282180cd1dd1
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-26 18:06:11 +0200

    Add back previously remove route to avoid routes

--- backend/src/api/routes/v1/user.routes.ts
+++ backend/src/api/routes/v1/user.routes.ts
@@ -3,15 +3,24 @@ import { isAuthenticated } from '../../middleware/isAuthenticated';
 import { isAuthorized } from '../../middleware/isAuthorized';
 import {
   addUser,
+  deleteUser,
   getUserById,
+  getUsers,
   loginUser,
-} from '../../controllers/user.controller';
+  logoutUser,
+  updateUser,
+} from '@/api/controllers/user.controller';
+import { userRole } from '@/api/middleware/userRole';
 
 const router = Router();
 
 router.post('/create', addUser);
 router.post('/login', loginUser);
 router.get('/user:id', isAuthenticated, isAuthorized, getUserById);
+router.get('/', isAuthenticated, isAuthorized, userRole('admin'), getUsers);
+router.get('/loguot', isAuthenticated, isAuthorized, logoutUser);
+router.put('/update:id', isAuthenticated, isAuthorized, updateUser);
+router.delete('/delete:id', isAuthenticated, isAuthorized, deleteUser);
 
 const userRoutes = router;
 
