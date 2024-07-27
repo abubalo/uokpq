63efbe1f1b5447eeadab3c13c22ea4e9e18e26e3
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-27 13:25:59 +0200

    Chnage javascript target to es2022

--- backend/tsup.config.ts
+++ backend/tsup.config.ts
@@ -4,10 +4,9 @@ export default defineConfig({
   entry: ['src/index.ts'],
   outDir: 'dist',
   format: ['cjs'],
-  target: 'es2019',
+  target: 'es2022',
   sourcemap: true,
   clean: true,
-  dts: true,
   splitting: false,
   minify: false,
   watch: false
