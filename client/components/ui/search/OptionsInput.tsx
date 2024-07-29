d4138fa336cdd4ee02a8d6311b5220109bfa35b6
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-29 21:46:26 +0200

    styles: Reduce the distance between text and input

--- client/components/ui/search/OptionsInput.tsx
+++ client/components/ui/search/OptionsInput.tsx
@@ -10,7 +10,7 @@ type OptionsProps<T = any> = {
     };
   
     return (
-      <div className="flex items-baseline justify-between w-full gap-10 ">
+      <div className="flex items-baseline justify-between w-full gap-4 ">
         <label htmlFor={joinLetters(name)} className="text-sm text-nowrap">
           {name}
         </label>
@@ -20,7 +20,7 @@ type OptionsProps<T = any> = {
           id={joinLetters(name)}
           className="w-full pt-1 text-sm border-b-2 border-gray-600 focus:outline-none placeholder:text-gray-400 dark:bg-transparent active dark:border-b-white dark:placeholder:text-gray-500 dark:text-gray-300 focus:border-b-blue-500"
           value={value}
-          onChange={(e) => onChange(e.target.value as T)}
+          onChange={(e) => onChange(e.target.value)}
         />
       </div>
     );
