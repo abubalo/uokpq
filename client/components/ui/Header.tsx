8c398cf8844ae06d8597338c38e56642fb09ab3d
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-29 21:42:52 +0200

    Add Avatar to header

--- client/components/ui/Header.tsx
+++ client/components/ui/Header.tsx
@@ -5,6 +5,7 @@ import { FiSun, FiMoon } from "react-icons/fi";
 import Search from "./search/Search";
 import Image from "next/image";
 import Link from "next/link";
+import Avatar from "../profile/Avatar";
 
 const Header = () => {
   const [darkMode, setDarkMode] = useState(false);
@@ -34,8 +35,10 @@ const Header = () => {
     setDarkMode((prevMode) => !prevMode);
   }, []);
 
+  const user = true;
+
   return (
-    <header className="sticky top-0 bg-white/30 dark:bg-neutral-800/30 backdrop-blur-md shadow-md z-50">
+    <header className="sticky top-0 bg-white/30 dark:bg-neutral-800/30 backdrop-blur-md z-50">
       <div className="container mx-auto px-4 py-3">
         <div className="flex items-center justify-between">
           {/* Logo */}
@@ -71,12 +74,19 @@ const Header = () => {
                 <FiMoon className="text-gray-600" size={30} />
               )}
             </button>
-            <Link
-              href="/login"
-              className="hidden bg-blue-500/80  hover:bg-blue-600/80 text-white px-4 py-2 rounded-full transition-colors md:block"
-            >
-              Login
-            </Link>
+            {user ? (
+              <Avatar
+                src="https://plus.unsplash.com/premium_photo-1683140618951-6232339fdb97?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt=""
+              />
+            ) : (
+              <Link
+                href="/login"
+                className="hidden border  px-4 py-2 rounded-full transition-colors md:block hover:bg-blue-500/70 "
+              >
+                Login
+              </Link>
+            )}
           </div>
         </div>
       </div>
