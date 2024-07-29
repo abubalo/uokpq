db0e2435091cb6c652fb19c4c19a37c3ea18889a
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-29 14:38:54 +0200

    Implement client-side caching using indexedDB

---
+++ client/indexedDB.ts
@@ -0,0 +1,59 @@
+const DB_NAME = "pdfCache";
+const STORE_NAME = "pds";
+const DB_VERSION = 1;
+
+const openDB = () => {
+  return new Promise<IDBDatabase>((resolve, rejects) => {
+    const request = indexedDB.open(DB_NAME, DB_VERSION);
+    request.onupgradeneeded = (event) => {
+      const db = (event.target as IDBOpenDBRequest).result;
+
+      if (!db.objectStoreNames.contains(STORE_NAME)) {
+        db.createObjectStore(STORE_NAME, { keyPath: "id" });
+      }
+    };
+
+    request.onsuccess = (event) => {
+      resolve((event.target as IDBOpenDBRequest).result);
+    };
+
+    request.onerror = (event) => {
+      rejects((event.target as IDBOpenDBRequest).result);
+    };
+  });
+};
+
+const getPDF = async (id: string) => {
+  const db = await openDB();
+
+  return new Promise<Blob | null>((resolve, reject) => {
+    const transaction = db.transaction(STORE_NAME, "readonly");
+    const store = transaction.objectStore(STORE_NAME);
+    const request = store.get(id);
+
+    request.onsuccess = (event) => {
+      resolve((event.target as IDBRequest).result?.blob || null);
+    };
+
+    request.onerror = (event) => {
+      reject((event.target as IDBRequest).error);
+    };
+  });
+};
+
+const savePDF = async (id: string, blob: Blob) => {
+  const db = await openDB();
+  return new Promise<void>((resolve, reject) => {
+    const transaction = db.transaction(STORE_NAME, "readwrite");
+    const store = transaction.objectStore(STORE_NAME);
+    const request = store.put({ id, blob });
+
+    request.onsuccess = () => {
+      resolve();
+    };
+
+    request.onerror = (event) => {
+      reject((event.target as IDBRequest).error);
+    };
+  });
+};
