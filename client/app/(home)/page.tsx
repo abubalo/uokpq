f359f3cdc62df3fe401b81a72fbaa1b63cf89531
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-30 19:10:05 +0200

    Add pagination component to paper page

--- client/app/(home)/page.tsx
+++ client/app/(home)/page.tsx
@@ -1,12 +1,28 @@
+
+"use client"
+
 import Header from "@/components/ui/Header";
 import Footer from "@/components/ui/Footer";
 import Papers from "@/components/papers/Papers";
+import Pagination from "@/components/ui/Pagination";
 
 export default function Home() {
+
+  const fetchData = async (page: number) =>{
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
   return (
     <main className="w-full min-h-dvh">
       <Header />
       <Papers />
+      <Pagination
+        totalPages={100}
+        fetchData={fetchData}
+      />
       <Footer />
     </main>
   );
