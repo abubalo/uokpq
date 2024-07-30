53d98600ed5eaa8f603db36c0f2dafe20021ae59
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-30 19:14:53 +0200

    Fix issue causing protected route to render

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,18 +1,15 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
 
-  const isPublicPath = ["/login", "/signup", "/paper", "/"].some(
-    (publicPath) => path.startsWith(publicPath)
+  const publicPaths = ["/login", "/register", "/paper", "/reset-password", "/"];
+  const isPublicPath = publicPaths.some((publicPath) =>
+    publicPath === "/" ? path === publicPath : path.startsWith(publicPath)
   );
 
   const token = request.cookies.get("Bearer")?.value || "";
-
-  console.log("Path:", path);
-  console.log("Token:", token);
-  console.log("isPublicPath:", isPublicPath);
 
   if (isPublicPath && token) {
     return NextResponse.redirect(new URL("/dashboard", request.nextUrl));
@@ -29,7 +26,7 @@ export const config = {
   matcher: [
     "/",
     "/login",
-    "/signup",
+    "/register",
     "/reset-password",
     "/paper/:path*",
     "/dashboard/:path*",
