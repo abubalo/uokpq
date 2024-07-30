2006d63522f8f1414a4e113f96d040c01623b34a
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-30 19:16:01 +0200

    Implement advance search state

--- client/stores/stateStore.ts
+++ client/stores/stateStore.ts
@@ -1,54 +1,43 @@
 import { create } from "zustand";
 
-
-
-
 type AdvanceOptiosStore = {
-  from: string;
-  to: string;
-  subject: string;
-  hasWords: string;
-  doesntHave: string;
-  size: string;
-  byteOption: number | string;
-  dateWithin: string | number;
-  hasAttachment: "yes" | "no";
-  doesntIncludeChats: "yes" | "no";
-  setFrom: (from: string) => void;
-  setTo: (to: string) => void;
-  setSubject: (subject: string) => void;
-  setHasWords: (hasWords: string) => void;
-  setDoesntHave: (doesntHave: string) => void;
-  setSize: (size: string) => void;
-  setByteOption: (byteOption: string | number) => void;
-  setDateWithin: (dateWith: string | number) => void;
-  setHasAttachment: (hasAttachment: "yes" | "no") => void;
-  setDoesntIncludeChats: (doesntIncludeChats: "yes" | "no") => void;
+  moduleName: string;
+  taughtBy: string;
+  department: string;
+  trimester: string;
+  date: string;
+  paperType: "exam" | "cat" | string;
+  modulesCode: string;
+  containsWords: number | string;
+  session: "day" | "evening" | "weekend" | string;
+  setModuleName: (moduleName: string) => void;
+  setTaugtBy: (taughtBy: string) => void;
+  setDepartment: (department: string) => void;
+  setTrimester: (trimester: string) => void;
+  setDate: (date: string) => void;
+  setPaperType: (paperType: "exam" | "cat") => void;
+  setModulesCode: (modulesCode: string) => void;
+  setContainsWords: (containsWords: string) => void;
+  setSession: (session: "day" | "evening" | "weekend" | string) => void;
 };
 
-
-
 export const useAdvanceSearchStore = create<AdvanceOptiosStore>((set) => ({
-  from: "",
-  to: "",
-  subject: "",
-  hasWords: "",
-  doesntHave: "",
-  size: "",
-  byteOption: "MB",
-  dateWithin: "",
-  hasAttachment: "no",
-  doesntIncludeChats: "yes",
-  setFrom: (from) => set({ from }),
-  setTo: (to) => set({ to }),
-  setSubject: (subject) => set({ subject }),
-  setHasWords: (hasWords) => set({ hasWords }),
-  setDoesntHave: (doesntHave) => set({ doesntHave }),
-  setSize: (size) => set({ size }),
-  setByteOption: (byteOption) => set({ byteOption }),
-  setDateWithin: (dateWithin) => set({ dateWithin }),
-  setHasAttachment: (hasAttachment) => set({ hasAttachment }),
-  setDoesntIncludeChats: (doesntIncludeChats) => set({ doesntIncludeChats }),
+  moduleName: "",
+  taughtBy: "",
+  department: "",
+  trimester: "",
+  date: "",
+  modulesCode: "",
+  containsWords: "",
+  session: "",
+  paperType: "",
+  setModuleName: (moduleName) => set({ moduleName }),
+  setTaugtBy: (taughtBy) => set({ taughtBy }),
+  setDepartment: (department) => set({ department }),
+  setTrimester: (trimester) => set({ trimester }),
+  setDate: (date) => set({ date }),
+  setModulesCode: (modulesCode) => set({ modulesCode }),
+  setContainsWords: (containsWords) => set({ containsWords }),
+  setSession: (session) => set({ session }),
+  setPaperType: (paperType) => set({ paperType }),
 }));
-
-
