db500b04aa0dde8366e31f41191d6e0e175d9901
Abubakar Balogun <abzynobalogun@gmail.com>
2024-07-30 19:13:46 +0200

    update bookmark icon color

--- client/components/papers/PaperCard.tsx
+++ client/components/papers/PaperCard.tsx
@@ -40,23 +40,19 @@ const PaperCard: React.FC<Props> = ({
   return (
     <div className="relative p-4 border border-neutral-400 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-200">
       <div className="relative w-full h-48">
-        <Image
-          src={src}
-          alt={alt}
-          layout="fill"
-          objectFit="cover"
-          className="rounded-t-lg"
-        />
+        <Image src={src} alt={alt} fill className="rounded-t-lg object-cover" />
       </div>
-      <div className="py-4 pb-2 space-y-4">
+      <div className="py-4 pb-2 space-y-6">
         <div className="space-y-1">
           <h2 className="text-xl font-semibold">{title}</h2>
-          <p className="dark:text-gray-300">{text}</p>
+          {/* <p className="dark:text-gray-300">{text}</p> */}
           <p className="dark:text-gray-300">
             Taught by: <span className="font-semibold">{lecturer}</span>
           </p>
+          <small className="dark:text-gray-400 font-semibold">
+            Date: {date}
+          </small>
         </div>
-        <small className="dark:text-gray-400 font-semibold">Date: {date}</small>
         <div className="space-x-2">
           {tags.length > 0
             ? tags.map((tag) => (
@@ -71,9 +67,13 @@ const PaperCard: React.FC<Props> = ({
         </div>
         <div className="flex justify-end items-center">
           <ToolTip text={isBookmarked ? "unbookmark" : "bookmark"}>
-            <span onClick={(e) => handleToggleBookmark(e)}>
-              {isBookmarked ? <FaBookmark /> : <FaRegBookmark />}
-            </span>
+            <button onClick={(e) => handleToggleBookmark(e)}>
+              {isBookmarked ? (
+                <FaBookmark className="text-yellow-500" />
+              ) : (
+                <FaRegBookmark />
+              )}
+            </button>
           </ToolTip>
         </div>
       </div>
