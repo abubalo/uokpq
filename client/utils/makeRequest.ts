e501640363f014865d65408a3f40168aabd69fc6
Abubakar Balogun <abzynobalogun@gmail.com>
2024-08-06 23:44:29 +0200

    Implement API request handler with structured error handling and generic response type

---
+++ client/utils/makeRequest.ts
@@ -0,0 +1,33 @@
+import { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
+
+interface ApiResponse<T> {
+  data: T | null;
+  error?: string | null;
+}
+
+export const makeApiRequest = async <T>(
+    axiosFn: (config?: AxiosRequestConfig) => Promise<AxiosResponse<T>>,
+    config?: AxiosRequestConfig
+  ): Promise<ApiResponse<T>> => {
+    try {
+      const response = await axiosFn(config);
+      return { data: response.data, error: null };
+    } catch (error) {
+      const errorMessage = error instanceof AxiosError 
+        ? handleApiError(error) 
+        : 'An unexpected error occurred';
+      console.error(errorMessage);
+      return { data: null, error: errorMessage };
+    }
+  };
+  
+
+const handleApiError = (error: AxiosError) => {
+  if (error.response) {
+    return `HTTP Error - Status Code: ${error.response.status}`;
+  } else if (error.request) {
+    return "No response received from the server.";
+  } else {
+    return "An error occurred while setting up the request.";
+  }
+};
