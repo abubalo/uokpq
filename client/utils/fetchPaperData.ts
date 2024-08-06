bbb0e192aea6e90ee6794d032a25ff232f55f3f9
Abubakar Balogun <abzynobalogun@gmail.com>
2024-08-06 23:45:16 +0200

    Implement paper data fetching

---
+++ client/utils/fetchPaperData.ts
@@ -0,0 +1,49 @@
+import { Paper } from "@/types";
+import { apiClient } from "./apiClient";
+import { makeApiRequest } from "./makeRequest";
+
+export function addPaper() {
+  return makeApiRequest<Paper>(() => apiClient.post("/paper"));
+}
+
+export function fetchPapers() {
+  return makeApiRequest<Paper[]>(() => apiClient.get("/papers"));
+}
+
+export function fetchPaperById(paperId: string) {
+  return makeApiRequest<Paper>(() => apiClient.get(`/papers/${paperId}`));
+}
+
+export function fetchPaperByUserId(userId: string) {
+  return makeApiRequest<Paper[]>(() => apiClient.get(`/papers/${userId}`));
+}
+
+export function searchPapers(queries: string) {
+  return makeApiRequest<Paper[]>(() =>
+    apiClient.get(`/papers/?query=${queries}`)
+  );
+}
+
+export function toggleBookmark(data: { userId: string; paperId: string }) {
+  return makeApiRequest<void>(() => apiClient.post("/papers/bookmark", data));
+}
+
+export function fetchBookmarks(userId?: string) {
+  return makeApiRequest<Paper[]>(() =>
+    apiClient.get(`/papers/bookmarks/${userId}`)
+  );
+}
+
+export function updatePaper(paper: Partial<Paper>) {
+  return makeApiRequest<Paper>(() => apiClient.put(`/papers`, paper));
+}
+export function archivePaper(paperId: string) {
+  return makeApiRequest<Paper[]>(() =>
+    apiClient.put(`/papers/achive/${paperId}`)
+  );
+}
+export function deletePaper(paperId: string) {
+  return makeApiRequest<Paper[]>(() =>
+    apiClient.delete(`/${paperId}`)
+  );
+}
