1a8a8852048786b770f15957067328553f951dc1
Abubakar Balogun <abzynobalogun@gmail.com>
2024-09-14 19:04:21 +0200

    Remove desktop search from header

--- client/components/ui/Header.tsx
+++ client/components/ui/Header.tsx
@@ -18,7 +18,9 @@ const Header = () => {
     setIsNavOpen(!isNavOpen);
   };
 
-  const handleSearch = (query: string) => {};
+  const handleSearch = (query: string) => {
+    
+  };
 
   const handleSearchFocus = (isFocused?: boolean) => {
     setOnsearchFocus(true || isFocused);
@@ -78,7 +80,7 @@ const Header = () => {
           </div>
 
           {/* Search Box */}
-          <Search />
+          {/* <Search /> */}
 
           {/* Right Side */}
           <div className="flex items-center space-x-4">
@@ -99,6 +101,7 @@ const Header = () => {
               <Avatar
                 src="https://plus.unsplash.com/premium_photo-1683140618951-6232339fdb97?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="User Avatar"
+                size="sm"
               />
             ) : (
               <Link
