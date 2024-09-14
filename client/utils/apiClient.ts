9ac6ccb671a9ce85b014994b8aa6c09551977043
Abubakar Balogun <abzynobalogun@gmail.com>
2024-09-14 19:27:56 +0200

    Format apiClient

--- client/utils/apiClient.ts
+++ client/utils/apiClient.ts
@@ -1,12 +1,11 @@
 import axios from "axios";
 
-
 export const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
   timeout: 1000,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
