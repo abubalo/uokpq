59f2757eeee7270ee2a99d6f54f4f697bc36671a
Abubakar Balogun <abzynobalogun@gmail.com>
2024-09-14 19:23:53 +0200

    Add unsplash image premium to remotePatterns

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         hostname: "images.unsplash.com",
+      },
+      {
+        hostname: "plus.unsplash.com",
+        pathname: "/premium_photo-*",
       },
       {
         hostname: "images.pexels.com",
