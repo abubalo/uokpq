d55563d788e4306bf628ef2ce9a0d593fa4ca820
Abubakar Balogun <abzynobalogun@gmail.com>
2024-09-14 18:20:22 +0200

    Improve Avatar component

--- client/components/profile/Avatar.tsx
+++ client/components/profile/Avatar.tsx
@@ -1,3 +1,6 @@
+import AvatarImage from "../avatar/AvatarImage";
+import AvatarFallback from "../avatar/AvatarFallback";
+
 type AvatarProps = {
   src: string;
   alt?: string;
@@ -5,33 +8,16 @@ type AvatarProps = {
   status?: "online" | "offline" | "away";
 };
 
-const Avatar: React.FC<AvatarProps> = ({ src, alt="", size = "md", status }) => {
-  const sizeClasses = {
-    sm: "w-8 h-8",
-    md: "w-12 h-12",
-    lg: "w-16 h-16",
-  };
-
-  const statusColors = {
-    online: "bg-green-500",
-    offline: "bg-gray-500",
-    away: "bg-yellow-500",
-  };
-
+const Avatar: React.FC<AvatarProps> = ({
+  src,
+  alt = "",
+  size = "md",
+  status,
+}) => {
   return (
     <div className="relative inline-block cursor-pointer">
-      <img
-        src={src}
-        alt={alt}
-        className={`${sizeClasses[size]} rounded-full object-cover border-2 bg-green-500 border-white shadow-sm`}
-      />
-      {status && (
-        <span
-          className={`absolute bottom-0 right-0 block rounded-full ${
-            sizeClasses[size] === "w-8 h-8" ? "w-2.5 h-2.5" : "w-3.5 h-3.5"
-          } ${statusColors[status]} ring-2 ring-white`}
-        />
-      )}
+      <AvatarImage {...{ src, alt, size }} />
+      <AvatarFallback size={size} status={status} />
     </div>
   );
 };
