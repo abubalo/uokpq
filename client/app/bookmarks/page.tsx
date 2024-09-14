3e00c0296ef56eba50c94f34c4082277c70a5879
Abubakar Balogun <abzynobalogun@gmail.com>
2024-09-14 19:37:21 +0200

    Move react how to the top

--- client/app/bookmarks/page.tsx
+++ client/app/bookmarks/page.tsx
@@ -1,25 +1,36 @@
+import Papers from "@/components/papers/Papers";
 import Footer from "@/components/ui/Footer";
 import Header from "@/components/ui/Header";
 import { useFetchBookmarks } from "@/hooks/usePaperQueries";
 import { useAuth } from "@/stores/userStatore";
 import React from "react";
-import Papers from "../paper/[paperUrl]/page";
 
 const Bookmarks = async () => {
-  const { user} = useAuth();
+  const { user } = useAuth();
 
-  if(!user){
-    return <>Logged in</>
+  
+  const { data, isLoading, error } = useFetchBookmarks(user?.id);
+  
+  if (!user) {
+    return <>Logged in</>;
   }
 
+  if (error) {
+    return <div>Error occured</div>;
+  }
 
-  const { data, isLoading, error } = useFetchBookmarks(user?.id);
+  if (isLoading) {
+    return <>Loading...</>;
+  }
 
-    const papers = [ {}]
+  if (!data) {
+    return <>No data</>;
+  }
+
   return (
     <main>
       <Header />
-      <Papers papers={papers} />
+      <Papers papers={data} />
       <Footer />
     </main>
   );
