659548e8b1b0118f7657a3de426a4691611e4fe0
Abubakar Balogun <abzynobalogun@gmail.com>
2024-09-24 14:28:36 +0200

    Add usePapers hooks

--- client/app/(home)/page.tsx
+++ client/app/(home)/page.tsx
@@ -7,38 +7,46 @@ import { usePapers } from "@/hooks/usePaperQueries";
 import Papers from "@/components/papers/Papers";
 import Search from "@/components/ui/search/Search";
 import { useState } from "react";
+import PaperCardSkeleton from "@/components/papers/PaperCardSkeleton";
 
 export default function Home() {
-  const { data, error, isLoading } = usePapers();
-  const [onSearchFocus, setOnsearchFocus] = useState(false);
+  const [page, setPage] = useState(1);
+  const { data, error, isLoading } = usePapers(page);
 
-  // if (error) {
-  //   return <div>Error occured</div>;
-  // }
-
-  // if (isLoading) {
-  //   return <>Loading...</>;
-  // }
-
-  const fetchData = async (page: number) => {
-    try {
-    } catch (error) {}
+  const fetchData = (pageNumber: number) => {
+    setPage(pageNumber);
   };
 
-  const handleSearch = (query: string) => {};
+  const renderContent = () => {
+    if (error) {
+      console.log(error);
+      return <div>Error loading papers</div>;
+    }
 
-  const handleSearchFocus = (isFocused?: boolean) => {
-    setOnsearchFocus(true || isFocused);
-  };
-  const handleSearchBlur = () => {
-    setOnsearchFocus(false);
+    if (!data) {
+      return <div>No papers found!</div>;
+    }
+
+    return (
+      <>
+        {isLoading ? (
+          <div className="grid grid-cols-1 p-4 md:grid-cols-2 lg:grid-cols-3 gap-8 md:p-0">
+            {[...Array(6)].map((_, i) => (
+              <PaperCardSkeleton key={i} />
+            ))}
+          </div>
+        ) : (
+          <Papers papers={data.papers} />
+        )}
+      </>
+    );
   };
 
   return (
     <main className="w-full h-auto min-h-dvh">
       <Header />
       <section className="container mx-auto flex gap-4 items-center justify-center flex-col h-96">
-        <h1 className="text-3xl font-semibold md:text-5xl ">
+        <h1 className="text-3xl font-semibold md:text-5xl">
           Find the Past CAT and Exam Papers
         </h1>
         <p>
@@ -47,8 +55,14 @@ export default function Home() {
         </p>
         <Search />
       </section>
-      <Papers papers={data} />
-      <Pagination totalPages={100} fetchData={fetchData} />
+      {renderContent()}
+      {data && data.papers.length > 0 && (
+        <Pagination
+          totalPages={data.totalPage}
+          currentPage={page}
+          onPageChange={fetchData}
+        />
+      )}
       <Footer />
     </main>
   );
