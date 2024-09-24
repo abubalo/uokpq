05e27b4ce0a758bdde3464bf513153517415595d
Abubakar Balogun <abzynobalogun@gmail.com>
2024-09-24 13:56:53 +0200

    Strip opanapi doc from paper route to promote readability

--- backend/src/api/routes/v1/paper.routes.ts
+++ backend/src/api/routes/v1/paper.routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import { isAuthenticated } from '@/api/middleware/isAuthenticated';
 import { isAuthorized } from '@/api/middleware/isAuthorized';
 import {
-  addPapper,
+  addPaper,
   archivePaper,
   deletePaper,
   getBookmarksByUser,
@@ -11,51 +11,19 @@ import {
   searchPapers,
   updatePaper,
 } from '@/api/controllers/paper.controler';
-import { toogleBookmark } from '@/api/models/paper.model';
+import { toggleBookmark } from '@/api/models/paper.model';
 import { userRole } from '@/api/middleware/userRole';
-import { deleteUser } from '@/api/controllers/user.controller';
 
 const router = Router();
 
 router.get('/', getPapers);
-router.get('/paper:id', getPaperById);
-router.post(
-  '/add-paper',
-  isAuthenticated,
-  isAuthorized,
-  userRole('admin'),
-  addPapper
-);
-router.put(
-  '/update-paper:id',
-  isAuthenticated,
-  isAuthorized,
-  userRole('admin'),
-  updatePaper
-);
-router.put(
-  '/archive-paper:id',
-  isAuthenticated,
-  isAuthorized,
-  userRole('admin'),
-  archivePaper
-);
-router.delete(
-  '/delete-paper:id',
-  isAuthenticated,
-  isAuthorized,
-  userRole('admin'),
-  deletePaper
-);
-router.delete(
-  '/delete-user:id',
-  isAuthenticated,
-  isAuthorized,
-  userRole('admin'),
-  deleteUser
-);
-router.post('/search', searchPapers);
-router.post('/bookmark', isAuthenticated, isAuthorized, toogleBookmark);
+router.get('/:id', getPaperById);
+router.post('/', isAuthenticated, isAuthorized, userRole('admin'), addPaper);
+router.put('/:id', isAuthenticated, isAuthorized, userRole('admin'), updatePaper);
+router.put('/archive/:id', isAuthenticated, isAuthorized, userRole('admin'), archivePaper);
+router.delete('/:id', isAuthenticated, isAuthorized, userRole('admin'), deletePaper);
+router.get('/search', searchPapers);
+router.post('/bookmark', isAuthenticated, isAuthorized, toggleBookmark);
 router.get('/bookmarks', isAuthenticated, isAuthorized, getBookmarksByUser);
 
 const paperRoutes = router;
