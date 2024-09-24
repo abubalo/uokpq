e35e4d0cf841820306f2a376b2082eb26de714b3
Abubakar Balogun <abzynobalogun@gmail.com>
2024-09-24 14:00:52 +0200

    Update path to docs yaml files

--- backend/swagger.ts
+++ backend/swagger.ts
@@ -1,22 +1,25 @@
 import { Express } from 'express';
 import swaggerJsDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
+import { join } from 'path';
 
 const swaggerOptions = {
   swaggerDefinition: {
     openapi: '3.0.0',
     info: {
       title: 'UokPQs API',
-      description: 'API Documentation',
+      description: 'API for managing University of Kigali past academic papers',
       version: '1.0.0',
       contact: {
         name: 'Abubakar Balogun',
         email: 'contact@abubalo.com',
       },
-      servers: [{ url: 'http://localhost:3000/api/v1' }],
+      servers: [{ url: 'http://localhost:5000/api/v1' }],
     },
   },
-  apis: ['./src/routes/*.ts'], 
+  apis: [
+    join(__dirname, './docs/*.yaml'),
+  ],
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
