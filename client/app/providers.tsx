26cf44513e1c5f1f5bbe7c8128be79dfd8801c74
Abubakar Balogun <abzynobalogun@gmail.com>
2024-09-27 22:30:10 +0200

    Add fetcher function to provider to fetcher to fetch user data

--- client/app/providers.tsx
+++ client/app/providers.tsx
@@ -1,9 +1,25 @@
 "use client";
-import React, { useState } from "react";
+import { useAuth } from "@/stores/userStore";
+import { User } from "@/types";
+import React, { useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 
-const Providers: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const Providers: React.FC<{
+  children: React.ReactNode;
+  initialUser?: User | null;
+}> = ({ children, initialUser }) => {
   const [queryClient] = useState(new QueryClient());
+  const { fetchProfile } = useAuth();
+
+  useEffect(() => {
+    if (initialUser) {
+      useAuth.setState({ user: initialUser });
+    } else {
+      fetchProfile();
+    }
+  }, [fetchProfile, initialUser]);
+
+
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
