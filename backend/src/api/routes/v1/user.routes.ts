d3afb4fd51b8c52afac5999e8dd67ceabfa834a2
Abubakar Balogun <abzynobalogun@gmail.com>
2024-09-29 12:53:47 +0200

    Update user route to match client request

--- backend/src/api/routes/v1/user.routes.ts
+++ backend/src/api/routes/v1/user.routes.ts
@@ -4,21 +4,20 @@ import { isAuthorized } from '../../middleware/isAuthorized';
 import {
   addUser,
   deleteUser,
+  getUser,
   getUserById,
-  getUsers,
   loginUser,
   logoutUser,
   updateUser,
 } from '@/api/controllers/user.controller';
-import { userRole } from '@/api/middleware/userRole';
 
 const router = Router();
 
 router.post('/create', addUser);
 router.post('/login', loginUser);
-router.post('/logout', isAuthenticated, isAuthorized, logoutUser);
+router.get('/', isAuthenticated, isAuthorized, getUser);
 router.get('/:id', isAuthenticated, isAuthorized, getUserById);
-router.get('/', isAuthenticated, isAuthorized, userRole('admin'), getUsers);
+router.post('/logout', isAuthenticated, isAuthorized, logoutUser);
 router.put('/:id', isAuthenticated, isAuthorized, updateUser);
 router.delete('/:id', isAuthenticated, isAuthorized, deleteUser);
 
