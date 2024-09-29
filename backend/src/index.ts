de5ccca9daed3a30586ca488298d0e99cd9bfdf1
Abubakar Balogun <abzynobalogun@gmail.com>
2024-09-29 12:45:03 +0200

    Improve the index.ts to handle graceful shutdown

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,15 +4,42 @@ import app from './app';
 import { Logging } from './utils/Logging';
 import { env } from '@config/env';
 import * as db from '@config/db';
+import { connectRabbitMQ } from './config/amqplib';
 
 const server = http.createServer(app);
-
-process.on('SIGINT', async () => {
-  await db.disconnect();
-  process.exit(0);
-});
-
 
 server.listen(env.SERVER_PORT, () => {
   Logging.log(`Server is running on localhost:${env.SERVER_PORT}`);
 });
+
+const gracefulShutdown = async (signal: string) => {
+  Logging.log(`Received ${signal}. Initiating graceful shutdown...`);
+
+  server.close(async (error) => {
+    if (error) {
+      Logging.error(`Error occured durring server shutdown: ${error}`);
+      process.exit(1);
+    }
+    try {
+      await db.disconnect();
+      Logging.log('Database disconnected successfully');
+
+      await connectRabbitMQ();
+      Logging.log('RabbitMq disconnected successfully');
+
+      Logging.log('All connections closed. Exiting process.');
+      process.exit(0);
+    } catch (shutdownError) {
+      Logging.error(`Error occured durring shutdown process. ${shutdownError}`);
+      process.exit(1);
+    }
+  });
+
+  setTimeout(() => {
+    Logging.warn('Graceful shutdown timed out. Forcing exit.');
+    process.exit(1);
+  }, 10000);
+};
+
+process.on('SIGINT', () => gracefulShutdown('SIGINT'));
+process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
