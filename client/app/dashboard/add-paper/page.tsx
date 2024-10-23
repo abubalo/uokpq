3615bc4f9ed0de042ee7ad3e951335e79a1ca4f1
Abubakar Balogun <abzynobalogun@gmail.com>
2024-10-23 13:55:11 +0200

    Create new page for adding paper

---
+++ client/app/dashboard/add-paper/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import PaperForm from "@/components/ui/PaperForm";
+
+export default function AddPaper() {
+  return (
+    <main className="flex justify-center items-center h-auto min-h-dvh my-10">
+      <PaperForm />
+    </main>
+  );
+}
