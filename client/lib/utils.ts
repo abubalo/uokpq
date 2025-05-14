e706cba2d7827f55d6e8833c4a062427acd6c7d5
Abubakar Balogun <abzynobalogun@gmail.com>
2025-05-14 17:01:32 +0200

    Fix frozen lock file

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function formatDate(date: string) {
+export function formatDate(date: string | number) {
   if (!date) return "Invalid date";
   
   const options: Intl.DateTimeFormatOptions = {
